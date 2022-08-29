c6e0fd6364319a23de18318ac46bce6dd1c8f254
Scrat1124 <111265350+Scrat112403@users.noreply.github.com>
2022-08-29 00:39:57 -0500

    tv

--- Televisores.py
+++ Televisores.py
@@ -1,144 +1,147 @@
-class Marca:
-    def __init__(self, nombre):
-        self.nombre=nombre
-
-    def getNombre (self):
-        return self.nombre
-
-    def setNombre (self, name):
-        self.nombre=name
-
-
-class TV:
-    def __init__(self, marca, canal, precio, estado, volumen, control):
-        self.marca= Marca
-        self.canal= canal
-        self.precio= precio
-        self.estado= estado
-        self.volumen= volumen
-        self.control=control
-
-    def __init__(self, marca, estado):
-        self.marca=Marca
-        self.estado=estado
-
-    canal=1
-    volumen=1
-    precio=500
-
-    def setMarca (self, marc):
-        self.marca=marc
-    def setControl (self, ctrl):
-        self.control= ctrl
-    def setPrecio (self, price):
-        self.precio=price
-    def setVolumen (self, vol):
-        self.volumen=vol
-    def setCanal (self, can):
-        self.canal=can
-
-    def getMarca (self):
-        return self.marca
-    def getControl (self):
-        return self.control
-    def getPrecio (self):
-        return self.precio
-    def getVolumen (self):
-        return self.volumen
-    def getCanal (self):
-        return self.canal
-
-
-    
-    numTV= 0
-    def nuevoTV():
-        numTV= numTV+1 
-
-    def turnOn (self, On):
-        self.estado=On
-    def turnOff (self, Off):
-        self.estado=Off
-    
-    def getEstado (self):
-        return self.estado
+def __init__():
+    class Marca:
+        def __init__(self, nombre):
+            self.nombre=nombre
         
-        if estado=="On":
-            def canalUp(self,canal):
-                if 120>canal>=1:
-                    self.canal= canal+1
-                else:
-                    self.canal= canal
+        def getNombre (self):
+            return self.nombre
+        
+        def setNombre (self, name):
+            self.nombre=name
+    
+    from Televisores import Marca
+    if __name__=="__main__":
+        class TV:
+            def __init__(self, marca, canal, precio, estado, volumen, control):
+                self.marca= Marca
+                self.canal= canal
+                self.precio= precio
+                self.estado= estado
+                self.volumen= volumen
+                self.control=control
+
+            def __init__(self, marca, estado):
+                self.marca=Marca
+                self.estado=estado
+
+            canal=1
+            volumen=1
+            precio=500
+
+            def setMarca (self, marc):
+                self.marca=marc
+            def setControl (self, ctrl):
+                self.control= ctrl
+            def setPrecio (self, price):
+                self.precio=price
+            def setVolumen (self, vol):
+                self.volumen=vol
+            def setCanal (self, can):
+                self.canal=can
+
+            def getMarca (self):
+                return self.marca
+            def getControl (self):
+                return self.control
+            def getPrecio (self):
+                return self.precio
+            def getVolumen (self):
+                return self.volumen
+            def getCanal (self):
+                return self.canal
+
+
+    
+            numTV= 0
+            def nuevoTV():
+                numTV= numTV+1 
+
+            def turnOn (self, On):
+                self.estado=On
+            def turnOff (self, Off):
+                self.estado=Off
+    
+            def getEstado (self):
+                return self.estado
             
-            def canalDown(self,canal):
-                if 120>=canal>1:
-                    self.canal= canal-1
-                else:
-                    self.canal= canal
+                if estado=="On":
+                    def canalUp(self,canal):
+                        if 120>canal>=1:
+                            self.canal= canal+1
+                        else:
+                            self.canal= canal
+            
+                    def canalDown(self,canal):
+                        if 120>=canal>1:
+                            self.canal= canal-1
+                        else:
+                            self.canal= canal
             
         
-        if estado=="On":
-            def volumenUp(self,volumen):
-                if 7>volumen>=0:
-                    self.volumen= volumen+1
-                else:
-                    self.volumen=volumen
+                if estado=="On":
+                    def volumenUp(self,volumen):
+                        if 7>volumen>=0:
+                            self.volumen= volumen+1
+                        else:
+                            self.volumen=volumen
     
-            def volumenDown(self,volumen):
-                if 7>=volumen>0:
-                    self.volumen= volumen-1
-                else:
-                    self.volumen=volumen
+                    def volumenDown(self,volumen):
+                        if 7>=volumen>0:
+                            self.volumen= volumen-1
+                        else:
+                            self.volumen=volumen
 
-
-class Control:
-    
-    def __init__(self, tv):
-        self.tv= TV
-    
-    def turnOn (self, On):
-        self.estado=On
-    def turnOff (self, Off):
-        self.estado=Off
-    
-    def getEstado (self):
-        return self.estado
+    from Televisores import TV
+    if __name__=="__main__":
+        class Control:
         
-        if estado=="On":
-            def canalUp(self,canal):
-                if 120>canal>=1:
-                    self.canal= canal+1
-                else:
-                    self.canal= canal
+            def __init__(self, tv):
+                self.tv= TV
+    
+            def turnOn (self, On):
+                self.estado=On
+            def turnOff (self, Off):
+                self.estado=Off
+    
+            def getEstado (self):
+                return self.estado
             
-            def canalDown(self,canal):
-                if 120>=canal>1:
-                    self.canal= canal-1
-                else:
-                    self.canal= canal
+                if estado=="On":
+                    def canalUp(self,canal):
+                        if 120>canal>=1:
+                            self.canal= canal+1
+                        else:
+                            self.canal= canal
+            
+                    def canalDown(self,canal):
+                        if 120>=canal>1:
+                            self.canal= canal-1
+                        else:
+                            self.canal= canal
         
-        if estado=="On":
-            def volumenUp(self,volumen):
-                if 7>volumen>=0:
-                    self.volumen= volumen+1
-                else:
-                    self.volumen=volumen
+                if estado=="On":
+                    def volumenUp(self,volumen):
+                        if 7>volumen>=0:
+                            self.volumen= volumen+1
+                        else:
+                            self.volumen=volumen
     
-            def volumenDown(self,volumen):
-                if 7>=volumen>0:
-                    self.volumen= volumen-1
-                else:
-                    self.volumen=volumen
+                    def volumenDown(self,volumen):
+                        if 7>=volumen>0:
+                            self.volumen= volumen-1
+                        else:
+                            self.volumen=volumen
 
-    def setCanal (self, can):
-        self.canal=can
+            def setCanal (self, can):
+                self.canal=can
         
 
-    def enlazar (self, televisor):
-        self.tv=televisor
-        self.TV.control: Control
+            def enlazar (self, televisor):
+                self.tv=TV
+                self.control: Control
     
-    def settv (self, tv):
-        self.tv=tv
+            def settv (self, tv):
+                self.tv=tv
 
-    def gettv (self):
-        return self.tv
+            def gettv (self):
+                return self.tv
