88d0f45afd13a8ece49dd32dfe87ab4c0cd4ce38
Scrat1124 <111265350+Scrat112403@users.noreply.github.com>
2022-08-30 01:38:15 -0500

    tv

--- televisores/control.py
+++ televisores/control.py
@@ -31,10 +31,10 @@ class Control:
             if 7>=self.tv.volumen>0:
                 self.tv.volumenDown()
 
-    def setCanal (self):
+    def setCanal (self,canal):
         if self.tv.estado==True:
             if 120>self.tv.canal>=1:
-                self.tv.canal()
+                self.tv.canal=canal
     
     def getCanal (self):
         return self.tv.canal
