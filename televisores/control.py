9600292775efda74c3499a9c1174ff08fbd363be
Scrat1124 <111265350+Scrat112403@users.noreply.github.com>
2022-08-30 01:22:19 -0500

    tv

--- televisores/control.py
+++ televisores/control.py
@@ -1,49 +1,49 @@
 from televisores.tv import TV
 class Control:
         
-            def __init__(self):
-                self.tv= None
+    def __init__(self):
+        self.tv= None
     
-            def turnOn (self):
-                self.tv.turnOn()
-            def turnOff (self):
-                self.tv.turnOff()
+    def turnOn (self):
+        self.tv.turnOn()
+    def turnOff (self):
+        self.tv.turnOff()
     
-            def getEstado (self):
-                return self.tv.estado
+    def getEstado (self):
+        return self.tv.estado
             
-            def canalUp(self):
-                if self.tv.estado==True:
-                    if 120>self.tv.canal>=1:
-                        self.tv.canalUp()
-            def canalDown(self):
-                if self.tv.estado==True:
-                    if 120>=self.tv.canal>1:
-                        self.tv.canalDown()
+    def canalUp(self):
+        if self.tv.estado==True:
+            if 120>self.tv.canal>=1:
+                self.tv.canalUp()
+    def canalDown(self):
+        if self.tv.estado==True:
+            if 120>=self.tv.canal>1:
+                self.tv.canalDown()
             
-            def volumenUp(self):
-                if self.tv.estado==True:
-                    if 7>self.tv.volumen>=0:
-                        self.tv.volumenUp()
+    def volumenUp(self):
+        if self.tv.estado==True:
+            if 7>self.tv.volumen>=0:
+                self.tv.volumenUp()
     
-            def volumenDown(self):
-                if self.tv.estado==True:
-                    if 7>=self.tv.volumen>0:
-                        self.tv.volumenDown()
+    def volumenDown(self):
+        if self.tv.estado==True:
+            if 7>=self.tv.volumen>0:
+                self.tv.volumenDown()
 
-            def setCanal (self, can):
-                self.tv.canal=can
+    def setCanal (self, can):
+        self.tv.canal=can
         
 
-            def enlazar (self,tv):
-                self.tv=tv
-                self.tv.setControl(self)
+    def enlazar (self,tv):
+        self.tv=tv
+        self.tv.setControl(self)
     
-            def setTV (self, tv):
-                self.tv=tv
+    def setTV (self, tv):
+        self.tv=tv
 
-            def getTV ():
-                return Control.tv
+    def getTV ():
+        return Control.tv
 
 
                 
