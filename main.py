f78f4df6c22a75c257e99eb7c2b03949132c9145
Scrat1124 <111265350+Scrat112403@users.noreply.github.com>
2022-08-29 22:53:47 -0500

    tv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from tv import TV
-from control import Control
-from marca import Marca
+from televisores.tv import TV
+from televisores.control import Control
+from televisores.marca import Marca
 
 if __name__ == "__main__":
     marca1 = Marca("Semsung")
