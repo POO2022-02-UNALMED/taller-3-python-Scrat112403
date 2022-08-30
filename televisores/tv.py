d38db9081f42761ab2e9d72696a5b54a51936d0d
Scrat1124 <111265350+Scrat112403@users.noreply.github.com>
2022-08-29 23:52:05 -0500

    TV

--- televisores/tv.py
+++ televisores/tv.py
@@ -11,7 +11,7 @@ class TV:
                 self.estado= estado
                 self.volumen= volumen
                 self.control=control
-                TV.numTV= TV.numTV+1
+                TV.numTV+= 1
 
     def __init__(self, marca, estado):
                 self.marca=Marca
